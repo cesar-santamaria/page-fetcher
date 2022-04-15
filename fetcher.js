d84d874acc509ea5eca209afe57b25f9407f99b6
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-04-15 11:33:17 -0500

    refactored code to account for edge cases

--- fetcher.js
+++ fetcher.js
@@ -1,7 +1,7 @@
 const request = require('request'); //npm package that allows for easier HTTP requests.
 const fs = require('fs'); //node package that allows to write content to a local file path.
 
-request('http://www.exampsle.edu/', function(error, response, body) {
+request('http://www.example.edu/', function(error, response, body) {
   if (error) { //if HTTP address is incorrect throw an error and end the process.
     console.log('WHOOPS something went wrong,',error);
     process.exit();
@@ -9,9 +9,17 @@ request('http://www.exampsle.edu/', function(error, response, body) {
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
 
   const content = body; //storing the recieved body request content in a variable
-  fs.writeFile('./index.html', content, err => {
+  const LOCAL_PATH = './index.html';
+  
+  if (fs.existsSync(LOCAL_PATH)) { // edge case: if file already exists exit out of process.
+    console.log('ERROR: file already exists.');
+    process.exit();
+  }
+
+  fs.writeFile(LOCAL_PATH, content, err => {
     if (err) return console.error(err);
-    //file written successfully
-    console.log('File written successfully!');
+    const stats = fs.statSync(LOCAL_PATH); // accessing stats on file written
+    const bytes = stats.size; // storing byte size downloaded
+    console.log(`Downloaded and saved ${bytes} bytes to ${LOCAL_PATH}`);//file written successfully
   });
 });
